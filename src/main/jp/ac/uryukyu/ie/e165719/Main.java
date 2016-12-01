ba4daf95680a3472c299612ea7f6d623f845ca82
Takahiro Iha <e165719@ie.u-ryukyu.ac.jp>
2016-12-01 16:05:39 +0900

    ステップ3

--- src/main/jp/ac/uryukyu/ie/e165719/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165719/Main.java
@@ -2,9 +2,10 @@ package jp.ac.uryukyu.ie.e165719;
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
+        String str = "3．14";
         try {
-            System.out.println(str.length());
+            double value = Double.parseDouble(str);
+            System.out.println(value);
         } catch (NullPointerException e){
             System.out.println("NullPointerExceptionが発生しました。");
             e.printStackTrace();
